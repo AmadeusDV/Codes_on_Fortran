5ce1860e7f8b27014931f126f6b85476624535d4
Amadeus <82341754+AmadeusDV@users.noreply.github.com>
2024-09-15 20:49:20 -0300

    Update RMQ_Chung.f90

--- CodesToStudy/RMQ_Chung.f90
+++ CodesToStudy/RMQ_Chung.f90
@@ -1,9 +1,31 @@
 PROGRAM RadiusMeanSquare
-       IMPLICIT NONE
+    IMPLICIT NONE
 
-	REAL Rmq, rkpp, rk, rc, rb
+        !Vamos explicitar aqui todas as variáveis que serão utilizadas
 
-	Rmq=1/8*(1/(rkpp**2) - (1/rk) + 2*(rc**2) + 
-     &((2*rc+rb)*(1+rkpp*rb))/rkpp - ((rb**3)*rkpp)/(3*(1+rb)))   
+        REAL Rmq, rKpp, rK, rC, rB
+        REAL V_0, E_0, hc, M, m_p, m_n
 
-      END
+        !Vamos declarar os valores das variáveis a serem utilizadas
+    
+        hc = 197.326
+        m_p = 938.272   ! m_p = 1.673x10^-27kg -> 938.272 Mev, massa do próton em mega eletronvolts, conversão realizada com a equação E=mc^2
+        m_n = 939.565   ! m_n = 1,675×10^−27kg -> 939.565 Mev, massa do neutron em mega eletronvolts, conversão realizada com a equação E=mc^2
+        V_0 = 40        ! V_0 = 40 Mev, representa a profundidade do poço retangular (modelo de gás do Fermi)
+        E_0 = 2.225     ! Energia de ligação E_0 = 2,225 Mev
+        rC = 0.4        ! rC = 0,8 fm, raio do próton
+        rB = 1.89       ! rB = 
+
+        !Precisaremos também das expressões da massa reduzida (M), de rKpp e rK,
+
+        M = (m_p*m_n)/(m_n+m_p)
+
+        rK = 1/hc * ((2*M*(V_0 - E_0))**(1/2))
+
+        rKpp = 1/hc * ((2*M*E_0)**(1/2))
+
+        !Vamos agora montar a equação a ser calculada, o resultado tem que ser Rmq = 0.8fm
+
+        Rmq = (1/8) * ( (1/rKpp**2) - (1/rK) + 2*(rC**2) + ((2*rC+rB)*(1+rKpp*rB))/rKpp - ((rB**3)*rKpp)/(3*(1+rB)))
+     
+END
